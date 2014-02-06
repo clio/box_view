79c73792fe2f93eed708383fb2c11c1620ee88e4
reillyforshaw <reilly.forshaw@goclio.com>
2014-02-06 14:40:41 -0800

    allow config in an initializer

--- lib/box_view/session.rb
+++ lib/box_view/session.rb
@@ -6,7 +6,15 @@ module BoxView
     attr_accessor :token
     
     def initialize(credentials={})
-      self.token = credentials[:token]
+      self.token = credentials[:token] || self.class.box_view_token
+    end
+
+    class << self
+      attr_accessor :box_view_token
+
+      def config
+        yield self
+      end
     end
 
   end
